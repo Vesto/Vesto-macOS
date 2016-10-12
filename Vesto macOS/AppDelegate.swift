2fcd044979f9f99b9cee7f6c0d0517874d1fac86
Nathan Flurry <bitbucket@nathanflurry.com>
2016-10-12 16:09:17 -0700

    Initial Commit

---
+++ Vesto macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Vesto macOS
+//
+//  Created by Nathan Flurry on 10/12/16.
+//  Copyright © 2016 Vesto. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Vesto macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Vesto macOS
+//
+//  Created by Nathan Flurry on 10/12/16.
+//  Copyright © 2016 Vesto. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
