7e306f2cafc916a6241a2b96ac87e21aa61cb12f
Nathan Flurry <bitbucket@nathanflurry.com>
2016-10-19 17:54:20 -0700

    - God addSubview working - Added QKError

--- Vesto macOS/main.js
+++ Vesto macOS/main.js
@@ -4,7 +4,7 @@ Logger.output("Parent " + parentView + "\n")
 parentView.rect = new Rect(new Point(10, 10), new Size(100, 100))
 
 var button = new View()
+button.rect = new Rect(new Point(0, 0), new Size(100, 100))
 //button.title = "hi"
 
-Logger.output(parentView.addSubview + "\n")
 parentView.addSubview(button)
